688a992d547d6ad386b53d42f534ddad1ffd2f52
ShadowSaber17 <159573997+ShadowSaber17@users.noreply.github.com>
2024-02-12 20:48:07 +0530

    Update To-Do list.py

--- To-Do list.py
+++ To-Do list.py
@@ -3,7 +3,8 @@ enter_task = ""
 view_task = ""
 task_complete = ""
 quit = ""
-task = [""]
+task = ""
+task_1 = []
 list = ["""
 Add task
 View task
@@ -13,10 +14,13 @@ print("What do you want to do? ")
 print(*list, sep=",")
 command = input("> ")
 command.upper()
-if command == "e":
-    task = input('Enter your task: ')
-    print(task)
-
+while command == "e":
+    if task == 'done':
+        print('These are all your tasks.')
+        break
+    else:
+        task_1 = [item for item in input("Enter your tasks: ").split()]
+        print(*task_1, sep="\n")
 
 
 
